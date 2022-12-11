5659f3539ef97a7cafe14a79c16e5f0f741aada3
= <=>
2022-12-10 17:18:06 -0700

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'doctrinal-mastery-restart-2',
+  webDir: 'www',
+  bundledWebRuntime: false
+};
+
+export default config;
